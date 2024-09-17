6cacde0a8226a4c8ef10e8e4b8da0aead1d71ddb
Your Name <eddykimutai@outlook.com>
2024-09-17 17:22:33 +0300

    Refactor models.py to add Customer and Order models

--- customers_orders/api/models.py
+++ customers_orders/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=100, unique=True)
+
+class Order(models.Model):
+    item = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    time = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
