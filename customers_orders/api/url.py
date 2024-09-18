89b5a5b9c55ba56b1f0f5ec18551161698bc90a7
Your Name <eddykimutai@outlook.com>
2024-09-18 15:34:00 +0300

    Refactor urls.py to include router.urls and register customer and order viewsets

--- customers_orders/api/url.py
+++ customers_orders/api/url.py
@@ -1,17 +1,12 @@
-
-from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
 
-# Cstom router & reg ur viewsets ..
+router = DefaultRouter()
+router.register(r'customers', views.CustomerViewSet)
+router.register(r'orders', views.OrderViewSet)
 
-router=DefaultRouter()       
-router.register('customers', views.CustomerViewSet)
-router.register('orders', views.OrderViewSet)
-
-# Include the router urls in urlpatterns (automatically generates urls for viewsets) 
 urlpatterns = [
-    path('',include('router.urls')),
+    path('', include(router.urls)),  # Include all router-generated URLs
 ]
