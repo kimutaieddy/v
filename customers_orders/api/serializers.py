4146587d84db3ae6edc366c411c7abe68c0b5f74
Your Name <eddykimutai@outlook.com>
2024-09-18 13:27:37 +0300

    Add CustomerSerializer for Customer model

--- customers_orders/api/serializers.py
+++ customers_orders/api/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import Customer, Order 
+
+class CustomerSerializer(serializers.modelsserializers):
+    class Meta:
+        model = Customer
+        fields = '__all__'
