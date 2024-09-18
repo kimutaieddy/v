3cd3a395612211bb1a42c0c39c835ab793d37d12
Your Name <eddykimutai@outlook.com>
2024-09-18 15:20:59 +0300

    Refactor urls.py to include router.urls and rest_framework.routers

--- customers_orders/customers_orders/urls.py
+++ customers_orders/customers_orders/urls.py
@@ -19,6 +19,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
+
+#cudtomize routers
+
+router=DefaultRouter()       
+router.register('customers', views.CustomerViewSet)
+router.register('orders', views.OrderViewSet)
+
+
 urlpatterns = [
     path('',include('router.urls')),
 ]
