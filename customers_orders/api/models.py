d50a79e51e62d7044614b45a3f33d37f0e52337e
Your Name <eddykimutai@outlook.com>
2024-09-18 11:57:25 +0300

    Refactor Order model to add __str__ method

--- customers_orders/api/models.py
+++ customers_orders/api/models.py
@@ -14,3 +14,5 @@ class Order(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     time = models.DateTimeField(auto_now_add=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
+def __str__(self):
+        return f"{self.item} ({self.amount})"
