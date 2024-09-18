1ddb11c7a57f6f3d9ca9435cc4a05f2ab8859be6
Your Name <eddykimutai@outlook.com>
2024-09-18 11:43:49 +0300

    Refactor Customer model to add created_at field

--- customers_orders/api/models.py
+++ customers_orders/api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Customer(models.Model):
     name = models.TextField() 
     code = models.CharField(max_length=100, unique=True,db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         index_together = [['name', 'code']]
