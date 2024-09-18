c5c40f086498d3bcd5714f74aea940280f2d9aaf
Your Name <eddykimutai@outlook.com>
2024-09-18 11:27:06 +0300

    Refactor models.py to add index_together for Customer model

--- customers_orders/api/models.py
+++ customers_orders/api/models.py
@@ -4,6 +4,9 @@ class Customer(models.Model):
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=100, unique=True)
 
+    class Meta:
+        index_together = [['name', 'code']]
+
 class Order(models.Model):
     item = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
