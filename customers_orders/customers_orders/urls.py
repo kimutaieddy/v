b5e3652106960477e47601d5deb174c2ba0f582b
Your Name <eddykimutai@outlook.com>
2024-09-18 15:31:28 +0300

    Refactor urls.py to include health check endpoint and API routes with namespace

--- customers_orders/customers_orders/urls.py
+++ customers_orders/customers_orders/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
-    path('api/', include('api.urls')),  # Include API routes under /api/
-    path('health/', include('health_check.urls'))  # Add health check endpoint
-    path('api/', include(('api.urls', 'api'), namespace='api')),  # Include API routes under /api/ with namespace
+    path('api/', include(('api.urls', 'api'), namespace='api')),  # Include API routes with namespace under /api/
+    path('health/', include('health_check.urls')),  # Add health check endpoint
 ]
