55a5723f6322428afb0710c3420f10e4623ca4ed
Your Name <eddykimutai@outlook.com>
2024-09-18 14:52:29 +0300

    Refactor views.py to import Customer and Order models and serializers

--- customers_orders/api/views.py
+++ customers_orders/api/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+from  .models import Customer, Order
+from .serializers import CustomerSerializer, OrderSerializer
+from rest_framework import viewsets
