9fb5ac799b514a622f03744e972630b826d4f029
Your Name <eddykimutai@outlook.com>
2024-09-18 14:58:43 +0300

    Refactor views.py to import Customer and Order models and serializers

--- customers_orders/api/views.py
+++ customers_orders/api/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 from  .models import Customer, Order
 from .serializers import CustomerSerializer, OrderSerializer
 from rest_framework import viewsets
+
+#ViewSets define the view behavior for the API views customers
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
