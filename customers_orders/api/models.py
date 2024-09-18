6ea4648acbfa06f4828a70c9033b63c1d34efcdd
Your Name <eddykimutai@outlook.com>
2024-09-18 11:31:29 +0300

    Refactor Customer model to add missing whitespace in name field

--- customers_orders/api/models.py
+++ customers_orders/api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Customer(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255) 
     code = models.CharField(max_length=100, unique=True)
 
     class Meta:
