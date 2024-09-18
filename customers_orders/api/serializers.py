9f3a4209fdfaf50ada4253f2414e204088bcec7a
Your Name <eddykimutai@outlook.com>
2024-09-18 15:51:51 +0300

    Refactor CustomerSerializer to use correct base class

--- customers_orders/api/serializers.py
+++ customers_orders/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Customer, Order 
 
-class CustomerSerializer(serializers.modelsserializers):
+class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['id', 'name', 'code' , 'created_at', 'updated_at']
