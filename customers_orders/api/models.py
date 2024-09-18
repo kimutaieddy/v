3474ec0fa9d9ab8ee47ee286cd8d93d4516ba84b
Your Name <eddykimutai@outlook.com>
2024-09-18 15:49:55 +0300

    Refactor Customer model to use CharField for name

--- customers_orders/api/models.py
+++ customers_orders/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 
 class Customer(models.Model):
-    name = models.TextField 
+    name = models.CharField(max_length=255)
     code = models.UUIDField(default=uuid.uuid4, unique=True, db_index=True)  
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
