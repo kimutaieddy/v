a05dc628beb2bc4978ab36ad894a3ae1b896aa3a
Your Name <eddykimutai@outlook.com>
2024-09-18 13:37:45 +0300

    Refactor CustomerSerializer fields and add OrderSerializer with nested CustomerSerializer

--- customers_orders/api/serializers.py
+++ customers_orders/api/serializers.py
@@ -5,3 +5,9 @@ class CustomerSerializer(serializers.modelsserializers):
     class Meta:
         model = Customer
         fields = ['id', 'name', 'code' , 'created_at', 'updated_at']
+
+class OrderSerializer(serializers.modelsserializers):
+    Customer = CustomerSerializer(read_only=True) # Nested serializer to show customer details(Like preview in Django Admin(Suggested))
+    class Meta:
+        model = Order
+        fields = ['id', 'item', 'amount', 'time', 'customer']
