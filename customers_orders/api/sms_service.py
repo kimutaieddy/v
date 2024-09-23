1ade7df4ff3ca74a059c0e98bf3b33a07820cae3
Your Name <eddykimutai@outlook.com>
2024-09-23 11:48:18 +0300

    Refactor sms_service.py to add send_sms function

--- customers_orders/api/sms_service.py
+++ customers_orders/api/sms_service.py
@@ -3,3 +3,8 @@ from django.conf import settings
 
 africastalking.initialize(settings.AFRICASTALKING_USERNAME, settings.AFRICASTALKING_API_KEY)
 sms = africastalking.SMS
+
+def send_sms(phone_numner, message):
+    try:
+        response=sms.send(message,[phone_numner])
+        print(f"SMS sent successfully: {response}")
