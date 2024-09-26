31ff2ce539891c1e2f4750b08fa5b672c401af18
Your Name <eddykimutai@outlook.com>
2024-09-26 11:16:02 +0300

    Refactor database configuration to use PostgreSQL instead of SQLite

--- customers_orders/customers_orders/settings.py
+++ customers_orders/customers_orders/settings.py
@@ -123,7 +123,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kurt',
         'USER': 'tits',
-        
+        'PASSWORD': '123',  
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
