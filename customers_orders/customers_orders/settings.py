7eca4ba9540670ddb68343c97dc34ec3c3cf8838
Kimutaieddy <131693884+kimutaieddy@users.noreply.github.com>
2024-10-15 07:38:11 +0000

    Add 'corsheaders' middleware to settings.py

--- customers_orders/customers_orders/settings.py
+++ customers_orders/customers_orders/settings.py
@@ -94,7 +94,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'customers_orders.urls'
